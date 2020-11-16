90a055bacf6b8abe2cc5ffbd576a470a87f7129d
ACaroline Luttringer <thisisraco@gmail.com>
2020-11-16 17:03:35 +0100

    devise setup and update of model user

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :address, presence: true
+end
