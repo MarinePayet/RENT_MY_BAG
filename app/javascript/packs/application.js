50ba4f35b34cc435ad19d34641a2e9c9a874031e
Chloe Chausson <chloechausson@gmail.com>
2020-11-19 11:36:44 +0100

    added calendar to the booking new page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import "../plugins/flatpickr"
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -29,6 +32,16 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
+ const bookingForm = document.getElementById('booking-form-div');
+
+ if (bookingForm) {
+    const bookings = JSON.parse(bookingForm.dataset.bookings);
+    flatpickr("#range_start", {
+      plugins: [new rangePlugin({ input: "#range_end"})],
+      minDate: "today",
+      inline: true,
+      dateFormat: "Y-m-d",
+      "disable": bookings,
+    })
+  }
 });
